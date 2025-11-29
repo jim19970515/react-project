ae00406da17648274c48e37ef36d5949d0d123ac
jim <f0920515972@gmail.com>
2025-11-30 00:01:21 +0800

    feat:navbar元件更新

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,3 +1,22 @@
+import { NavLink } from "react-router-dom";
 export function NavBar() {
-  return <nav className="h-16 w-full bg-red-500"></nav>;
+  const navItems = [
+    { label: "首頁", path: "/" },
+    { label: "房間", path: "/room" },
+    { label: "設施與服務", path: "/services" },
+    { label: "品牌", path: "/brand" },
+    { label: "聯繫我們", path: "/contact" }
+  ];
+
+  return (
+    <>
+      <nav className="flex justify-center items-center gap-4 h-16 w-full bg-red-500">
+        {navItems.map((item) => (
+          <NavLink key={item.path} to={item.path}>
+            {item.label}
+          </NavLink>
+        ))}
+      </nav>
+    </>
+  );
 }
