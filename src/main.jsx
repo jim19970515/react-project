f1a851f8dd72e91525c823536c116150af0af97b
jim <f0920515972@gmail.com>
2025-11-29 15:16:57 +0800

    feat:修改根元件、與App顯示內容配置

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+//StrictMode 是 React 的開發工具，用于在開發階段檢測潜在問题
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+//首屏進入也懶加
+
+const app = createRoot(document.getElementById("root"));
+app.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
