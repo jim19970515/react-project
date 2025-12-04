5703b96f874ddc2d2c386cdd4539849914365fed
jim <f0920515972@gmail.com>
2025-12-05 00:00:44 +0800

    feat:主頁切版調整

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+import { TfiAlignLeft } from "react-icons/tfi";
 // components
 //header
 import { NavBar } from "./components/navBar";
@@ -9,8 +10,13 @@ function App() {
   return (
     <>
       <header>
-        <div className="flex justify-center items-center">
-          <img className="h-24" src="/src/assets/img/logo.png" alt="logo" />
+        <div className="flex justify-between items-center p-2">
+          <img className="h-12" src="/src/assets/img/logo.png" alt="logo" />
+          <div className="flex items-center gap-4 text-xl text-[#525151]">
+            <NavLink>繁中</NavLink>
+            <NavLink>EN</NavLink>
+            <TfiAlignLeft className="text-3xl ml-4" />
+          </div>
         </div>
         <NavBar />
         <SwiperComponent />
@@ -20,7 +26,21 @@ function App() {
           <Outlet />
         </Suspense>
       </main>
-      <footer></footer>
+      <footer>
+        <div className="fixed bottom-0.5  p-2  text-white ">
+          <div className="flex items-center gap-4 bg-[#A12245] rounded-full px-4 py-3">
+            <NavLink>
+              <button>AI客服</button>
+            </NavLink>
+            <NavLink>
+              <button>預約</button>
+            </NavLink>
+            <NavLink>
+              <button>加入會員</button>
+            </NavLink>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
