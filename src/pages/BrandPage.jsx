3720e9d31c8f91c5bc5dfa0c4e3240644169fb4b
jim <f0920515972@gmail.com>
2025-12-10 22:53:20 +0800

    feat: BrandPage品牌頁面內容新增

--- src/pages/BrandPage.jsx
+++ src/pages/BrandPage.jsx
@@ -1,4 +1,62 @@
 //品牌頁面
 export default function BrandPage() {
-  return <div>BrandPage</div>;
+  return (
+    <div className="w-full">
+      <div className="w-full h-40 bg-[#A12245]/80 flex justify-center items-center">
+        <img
+          src="/src/assets/img/logo.png"
+          alt="room"
+          className="px-2 h-16 py-2 object-contain bg-white rounded-lg shadow-md shadow-[#ef98b1]"
+        />
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">品牌小故事</div>
+      <div className="flex flex-col justify-start gap-2 my-4 px-2 text-center text-sm text-[#525151]">
+        <p className="font-bold text-lg">瑞諾瓦酒店（RENOVA Hotel） 的誕生</p>
+        <p>源於一個簡單卻深刻的理念</p>
+        <p>「Renew & Nova：在熟悉的城市中，找到全新的自己。」</p>
+        <p>
+          「RENOVA」由 Renew（重生）＋ Nova（新星）
+          延伸而來，象徵著旅人無論因工作、探索、療癒或度假而前來，都能在這裡擁有一段煥然一新的開始。
+        </p>
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">品牌精神</div>
+      <div className="flex flex-col justify-start gap-2 my-4 px-2 text-center text-sm text-[#525151]">
+        <p className="font-bold text-lg">城市中的精品度假光點</p>
+        <p>瑞諾瓦相信，精品不是冷漠的奢華，而是 剛剛好的精緻、恰到好處的貼心。</p>
+        <p>因此我們打造的空間有三個核心精神：</p>
+        <p className="font-bold">靈感設計 Inspiration</p>
+        <p>融合現代城市線條與柔和光影， 每一個角落都散發著低調優雅，像是一幅隨時啟動想像力的畫。</p>
+        <p className="font-bold">靈感設計 Inspiration</p>
+        <p>空間大量運用深紅 #A12245 作為品牌靈魂色， 象徵熱情與力量，也象徵長途旅程後，那杯讓人安心的暖茶。</p>
+        <p className="font-bold">城市綠洲 Urban Oasis</p>
+        <p>我們把「度假」搬進城市中心， 無需奔波，就能在泳池邊、落地窗前、精品床系之上，輕易地放鬆自己。</p>
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">品牌精神</div>
+      <div className="flex flex-col justify-start gap-2 my-4 px-2 text-center text-sm text-[#525151]">
+        <div className="font-bold text-lg">為旅人留下一段「值得收藏的時光」</div>
+        <p>瑞諾瓦的「瑞」，象徵吉兆、幸運，是旅途開啟前最美好的祝福。</p>
+        <p>諾：代表承諾，是我們對服務細節永不妥協的態度。</p>
+        <p>瓦：取其「家屋」寓意，象徵旅途歸屬與庇護。</p>
+        <p>三字結合</p>
+        <p>形成瑞諾瓦獨有的氣質—— 「讓旅人放心的承諾、讓旅途幸運的開始、讓城市旅行也能像回家。」</p>
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">我們的旅人</div>
+      <div className="flex flex-col justify-start gap-2 my-4 px-2 text-center text-sm text-[#525151]">
+        <p>瑞諾瓦不只為觀光客而存在，也為</p>
+        <p>✔ 需要片刻安靜的城市居民</p>
+        <p>✔ 追求質感生活的年輕世代</p>
+        <p>✔ 期待專業卻不浮誇服務的商務旅人</p>
+        <p>✔ 喜歡靈感空間、美好視覺與儀式感的人</p>
+        <p className="font-bold text-lg">準備了一個心境可以輕鬆「切換頻道」的地方。</p>
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">❤️瑞諾瓦想傳達的核心</div>
+      <div className="flex flex-col justify-start gap-2 my-4 px-2 text-center text-sm text-[#525151]">
+        <p>「不離城市，卻像遠離日常。」</p>
+        <p>在這裡，旅行不是逃離，而是重新整理自己。</p>
+        <p className="font-bold text-lg">這，就是 瑞諾瓦酒店 RENOVA Hotel</p>
+        <p>一處會讓你想再次回來的精品度假城市據點。</p>
+      </div>
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 h-8 rounded-t-2xl"></div>
+    </div>
+  );
 }
