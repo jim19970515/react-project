b624e8ec13a5ce830273299dd33543519aeec129
jim <f0920515972@gmail.com>
2025-12-10 23:38:19 +0800

    fix:補上圖片

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,12 +1,11 @@
 //設施與服務頁
+// images
+import servicesBannerImg from "../assets/img/services-banner.png";
+
 export default function ServicesPage() {
   return (
     <div className="w-full">
-      <img
-        src="/src/assets/img/services-banner.png"
-        alt="room"
-        className="w-full h-40  shadow-lg py-2 bg-[#A12245]/80  object-cover"
-      />
+      <img src={servicesBannerImg} alt="room" className="w-full h-40  shadow-lg py-2 bg-[#A12245]/80  object-cover" />
       <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">設施與服務</div>
       <div className="mt-4"></div>
     </div>
