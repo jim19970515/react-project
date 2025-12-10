efd2f69b81770cefca4b3ebc0e36e7858bc3e7c8
jim <f0920515972@gmail.com>
2025-12-10 22:54:09 +0800

    feat:聯絡我們內容頁面調整

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,3 +1,13 @@
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <div className="w-full">
+      <img
+        src="/src/assets/img/contact-banner.png"
+        alt="room"
+        className="w-full h-40  shadow-lg py-2 bg-[#A12245]/80  object-auto"
+      />
+      <div className="text-2xl font-bold text-[#A12245] text-center bg-[#A12245]/20 py-1">聯絡我們</div>
+      <div className="mt-4"></div>
+    </div>
+  );
 }
