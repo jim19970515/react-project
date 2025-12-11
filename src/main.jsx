3b61109a075efa1389759c0d38cbd7f2fcb1e547
jim <f0920515972@gmail.com>
2025-12-11 23:43:35 +0800

    feat:加入初始化GA

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 //StrictMode 是 React 的開發工具，用于在開發階段檢測潜在問题
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
+//router
 import router from "./router";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+// analytics
+import { initGA4 } from "./libs/analytics";
+// 初始化 GA4
+initGA4();
+
 const app = createRoot(document.getElementById("root"));
 app.render(
   <StrictMode>
