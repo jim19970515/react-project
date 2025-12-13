cd52885e1f2264c38776482ee4b98e12eee97dde
jim <f0920515972@gmail.com>
2025-12-13 14:07:18 +0800

    feat: 新增ga4 event fn

--- src/libs/analytics.jsx
+++ src/libs/analytics.jsx
@@ -3,3 +3,6 @@ import ReactGA from "react-ga4";
 export const initGA4 = () => {
   ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 };
+export const trackGa4Event = (eventName, params = {}) => {
+  ReactGA.event(eventName, params);
+};
