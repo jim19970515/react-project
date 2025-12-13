44e8ffb1846baf8acbdd0e2f18fab02d7fa76187
jim <f0920515972@gmail.com>
2025-12-13 14:06:49 +0800

    feat:將form拆元件

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,69 +1,14 @@
-//首頁
+import { RoomForm } from "../components/roomForm";
 import { SwiperComponent } from "../components/swiper";
-import { Col, Row } from "antd";
-import { DatePicker, Select, Button } from "antd";
-
-// dayjs
-import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat";
-import localeData from "dayjs/plugin/localeData";
-import weekday from "dayjs/plugin/weekday";
-dayjs.extend(customParseFormat);
-dayjs.extend(localeData);
-dayjs.extend(weekday);
-
 // images
 import newMessageImg from "../assets/img/new-message.png";
 
-const adultOptions = [
-  {
-    value: 1
-  },
-  {
-    value: 2
-  },
-  {
-    value: 3
-  },
-  {
-    value: 4
-  }
-];
-const childOptions = [
-  {
-    value: 1
-  },
-  {
-    value: 2
-  }
-];
 export default function HomePage() {
-  const today = dayjs();
-  const tomorrow = dayjs().add(1, "day");
-
-  const dateFormat = "YYYY-MM-DD";
-
   return (
     <div>
       <SwiperComponent />
       <div>
-        <Row justify="center" gutter={[0, 16]} className="py-6">
-          <Col span={22}>
-            <DatePicker className="w-full h-12" defaultValue={today} format={dateFormat} />
-          </Col>
-          <Col span={22}>
-            <DatePicker className="w-full h-12" defaultValue={tomorrow} format={dateFormat} />
-          </Col>
-          <Col span={11}>
-            <Select className="w-full h-12" options={adultOptions} placeholder="成人" />
-          </Col>
-          <Col span={11}>
-            <Select className="w-full h-12" options={childOptions} placeholder="孩童" />
-          </Col>
-          <Col span={22}>
-            <Button className="w-full !h-10 !bg-[#A12245] !text-white  active:!bg-[#851533]">查詢</Button>
-          </Col>
-        </Row>
+        <RoomForm />
       </div>
       <div className="w-full">
         <h2 className="text-2xl font-bold w-fit px-2 mx-auto bg-white text-[#A12245] rounded-lg">最新消息</h2>
